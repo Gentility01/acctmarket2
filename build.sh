1c1c81812a9b3e7be000b10f5c7e12ead796aac0
Gentility01 <mastergentility5@gmail.com>
2024-06-26 20:26:38 +0100

    updates

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip install -r requirements/base.txt
+pip install -r requirements/production.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
